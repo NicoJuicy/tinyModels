8097136aec7b81c1c36bc6aa1dfd0b3ee30aeeab
Teodor Fratiloiu <teodor.fratiloiu@gmail.com>
2020-06-05 10:12:57 +0200

    Update inference.py

--- inference.py
+++ inference.py
@@ -1,26 +1,20 @@
-import tensorflow.keras as keras
 import tensorflow as tf
-from numpy import shape, load, save
 import numpy as np
-from sklearn.metrics import accuracy_score
-from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import Normalizer
-from sklearn.svm import SVC
 
 platform = "keras"
-platform = "tflite"
+# platform = "tflite"
 
 # For running full-size model inference
 if platform == "keras":
     model_path = "model_full.h5"
-    model = keras.models.load_model(model_path)
+    model = tf.keras.models.load_model(model_path)
 
     # inference prototype: result = model.predict(image)
 
     dataset_file = "export/dataset.npy"
-    dataset = load(dataset_file)
+    dataset = np.load(dataset_file)
     result = model.predict(dataset)
-    # print(shape(dataset))
+    # print(np.shape(dataset))
     print(result)
 
 # For running TFlite reduced-size model inference
@@ -35,8 +29,10 @@ elif platform == "tflite":
 
     # Test the model on random input data.
     input_shape = input_details[0]['shape']
-    input_data = np.array(np.random.random_sample(input_shape), dtype=np.float32)
-    interpreter.set_tensor(input_details[0]['index'], input_data)
+    # input_data = np.array(np.random.random_sample(input_shape), dtype=np.float32)
+    input_dataset = np.load("export/dataset.npy")
+    input_data = np.array(input_dataset, dtype=np.float32)
+    # interpreter.set_tensor(input_details[0]['index'], input_data)
 
     interpreter.invoke()
 
