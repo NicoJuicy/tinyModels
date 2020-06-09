2e1e0f31cad9075b8396c5175d766c5796c6f077
Teodor Fratiloiu <teodor.fratiloiu@gmail.com>
2020-06-09 19:19:05 +0200

    some succesful tflite inference

--- inference.py
+++ inference.py
@@ -2,10 +2,11 @@ import tensorflow as tf
 import numpy as np
 import os
 from matplotlib import pyplot
+import cv2
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 
-platform = "keras"
-# platform = "tflite"
+# platform = "keras"
+platform = "tflite"
 
 
 # For running full-size model inference
@@ -37,16 +38,6 @@ if platform == "keras":
     print(f"Positives: {positives / (positives + negatives) * 100}%")
     print(f"Negatives: {negatives / (positives + negatives) * 100}%")
 
-    # img1 = load_img("dataset/faces/abb8c22e6900.jpg")
-    # img_array = np.empty((1, 32, 32, 3), dtype=np.uint8)
-    # img = img_to_array(img1)
-    # img_array[0,:,:,:] = img
-    # result = model.predict(img_array)
-
-    # if result[0,0] == 0 and result[0,1] == 1:
-    #     print("Face positive")
-    # elif result[0,0] == 1 and result[0,1] == 0:
-    #     print("Face negative")
 
 # For running TFlite reduced-size model inference
 elif platform == "tflite":
@@ -60,14 +51,28 @@ elif platform == "tflite":
 
     # Test the model on random input data.
     input_shape = input_details[0]['shape']
-    # input_data = np.array(np.random.random_sample(input_shape), dtype=np.float32)
-    input_dataset = np.load("export/dataset.npy")
-    input_data = np.array(input_dataset, dtype=np.float32)
-    # interpreter.set_tensor(input_details[0]['index'], input_data)
 
-    interpreter.invoke()
+    positives = 0
+    negatives = 0
 
-    # The function `get_tensor()` returns a copy of the tensor data.
-    # Use `tensor()` in order to get a pointer to the tensor.
-    output_data = interpreter.get_tensor(output_details[0]['index'])
-    print(output_data)
+    for one_image in os.listdir("dataset/test"):
+        if (positives + negatives) < 200:
+
+            img1 = load_img(f"dataset/test/{one_image}", target_size=(32,32))
+            input_data = np.empty((1, 32, 32, 3), dtype=np.float32)
+            img = img_to_array(img1)
+            input_data[0, :, :, :] = img
+            # result = model.predict(img_array)
+            interpreter.set_tensor(input_details[0]['index'], input_data)
+            interpreter.invoke()
+            output_data = interpreter.get_tensor(output_details[0]['index'])
+            print(one_image, output_data)
+
+        if output_data[0, 0] >= 0.9 and output_data[0, 1] <= 0.1:
+           positives += 1
+        elif output_data[0, 0] <= 0.1 and output_data[0, 1] >= 0.9:
+            negatives += 1
+
+    print(f"Positives: {positives / (positives + negatives) * 100}%")
+    print(f"Negatives: {negatives / (positives + negatives) * 100}%")
+
