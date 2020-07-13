06f9ef9bad45263de814cedfa0e00856cfa356ec
Teodor Fratiloiu <teodor.fratiloiu@gmail.com>
2020-07-13 17:39:26 +0200

    Update webcam.py

--- webcam.py
+++ webcam.py
@@ -19,7 +19,7 @@ fontColor_red = (0,0,128)
 fontColor_yellow = (255,255,0)
 lineType = 2
 
-model_path = "trained_models/squeezenet_opt.tflite"
+model_path = "squeezenet_opt.tflite"
 input_image_dim = (dimension[0], dimension[1], 1 if grayscale else 0)
 print(f"Working with images in {input_image_dim}")
 
